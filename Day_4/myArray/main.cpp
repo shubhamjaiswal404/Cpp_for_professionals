ce03df06146b414d9bbf6ec814ee8ffaf801390e
shubhamjaiswal404 <shubhamjaiswal08022003@gmail.com>
2025-09-04 15:57:22 +0530

    Adding main.cpp file of myVec_Array myArray myIntArray

---
+++ Day_4/myArray/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
+#include <array>
+//#include <vector>
+
+using namespace std;
+
+int main()
+{
+   // int                     myInt[100000];
+       std::array<int,100000>  myArr;
+    //   std::vector<int>        myVec;
+
+    srand(time(NULL));
+
+    chrono::high_resolution_clock::time_point timeStart = chrono::high_resolution_clock::now();
+
+
+    for(int i=0; i<100000; i++)
+    {
+
+           myArr[i] = rand()%100 + 1 ;
+
+    }
+
+    chrono::high_resolution_clock::time_point timeEnd = chrono::high_resolution_clock::now();
+
+    chrono::milliseconds execTime = chrono::duration_cast<chrono::milliseconds>(timeEnd - timeStart);
+
+    cout<<"myArr array running time is "<< execTime.count() <<" ms" <<endl;
+
+    return 0;
+}
