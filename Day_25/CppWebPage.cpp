c5c1920479a0ee1f0f2699480f30778e0c010198
shubhamjaiswal404 <shubhamjaiswal08022003@gmail.com>
2025-09-18 00:49:26 +0530

    C++ Programming on Linux :: C++ Web : Hello World Web Page

---
+++ Day_25/CppWebPage.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+/*
+   http://localhost/cgi-bin/CppWebPage.cgi
+*/
+void printWeb()
+{	
+   std::cout << "Content-type:text/html\n\n";
+   std::cout << "<html>\n";
+   std::cout << "<head>\n";
+   std::cout << "<title> Hello World - First C++ Web Program </title> \n";
+   std::cout << "</head> \n"; 
+   std::cout << "<body>\n";
+   std::cout << "<div style=\"width: 100%; font-size : 36px; font-weight: bold; text-align: center; color: blue;\">\n ";
+   std::cout << "<h2>Hello World ! </h2> \n";
+   std::cout << "This is my C++ Web Programming Test Page  \n";
+   std::cout << "</div> \n";
+   std::cout << "</body>\n";
+   std::cout << "</html>\n";
+
+}
+
+int main()
+{
+ 	printWeb();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
