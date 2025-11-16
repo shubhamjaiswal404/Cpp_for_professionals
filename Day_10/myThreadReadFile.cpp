72cf5bf095eb020c795e9d76fe355cf10019f4e9
shubhamjaiswal404 <shubhamjaiswal08022003@gmail.com>
2025-11-16 14:45:55 +0530

    Removing main.cpp because '00001 Hello world I am here\n' as 28 bytes in eachline , 27 character and 1 newline , and removing comments , adding myThreadReadFile.cpp and Makefile

--- Day_10/myThreadReadFile.cpp
+++ Day_10/myThreadReadFile.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include<chrono>
-#include<pthread.h>
-#include<stdio.h>
-#include<string.h>
+#include <chrono>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 #define MAX_THREADS     2
@@ -21,24 +21,26 @@ void *ReadFile(void *tParam)
     std::string line;
     int i , initPos;
 
-    initPos = mData->t_id * 10000 * 27;
+    initPos = mData->t_id * 10000 * 28;
 
     ifstream myFile(mData->t_FileName);
+
     if(myFile.is_open())
     {
         i=0;
         myFile.seekg(initPos,ios::beg);
-        while (getline(myFile , line) && i<10000)
+        while (getline(myFile , line) && i < 10000)
         {
             i++;
-            if(i== 1|| i== 10000)
+            if(i == 1 || i == 10000)
+	    {
                 std::cout<< "Thread  " <<mData->t_id<<" : "<< line <<std::endl;
-
+	    }
         }
-        std::cout<< line <<std::endl;
         myFile.close();
     }
-    else{
+    else
+    {
         std::cout<<"can not open file example.txt for reading "<<std::endl;
     }
 
@@ -51,8 +53,8 @@ int main(int argc, char *argv[])
     pthread_t threads[MAX_THREADS];
     struct threadParam mTD[MAX_THREADS];
     int j , rc;
-    std::string line;
-    int i;
+    
+
     if( argc !=2 )
     {
         std::cout << "Usage: myReadFile <file-Name>"<<std::endl;
@@ -66,7 +68,8 @@ int main(int argc, char *argv[])
         memcpy(mTD[j].t_FileName, argv[1], strlen(argv[1]));
         rc = pthread_create(&threads[j],NULL,ReadFile,(void*)&mTD[j]);
 
-        if(rc){
+        if(rc)
+	{
             cout<<"Error : unable to create thread,"<< rc <<endl;
             exit(-1);
         }
